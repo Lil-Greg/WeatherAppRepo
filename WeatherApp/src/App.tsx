9ca076364980e57c2fb9973ff17530002907eb52
Lil-Greg <gregorywilliams1289@gmail.com>
2024-04-27 14:15:21 -0400

    Added useWeatherApi to App
    Now need to add an input to test the input stuff, and add conditional statements to change the state of the app... to look good

--- WeatherApp/src/App.tsx
+++ WeatherApp/src/App.tsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function App() {
+  const APIKey = import.meta.env.VITE_API_KEY;
+
+  function useSearchWeather(city: string) {
+    const [weather, setWeather] = useState({});
+    useEffect(() => {
+      async function fetchData() {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`);
+        const data = await response.json();
+        setWeather(data);
+      }
+      fetchData();
+    }, [city])
+    return weather;
+  }
+  // Try to use weather so I can use the data
+  // weather is in celsius, also add a math.floor to get whole number
+
+  console.log(useSearchWeather("new york"))
+
+  //const cityInput = document.querySelector("input .city").target.value
+  //const weather = useSearchPokemon(cityInput)
+
 
   return (
     <>
-      {/* Code */}
+
     </>
   )
 }
