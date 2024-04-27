3f862524af3f8ef7ca8bd1255f041623064da989
RyderJo <ryjoyner10@gmail.com>
2024-04-27 11:31:17 -0400

    Search

--- README.md
+++
@@ -1,2 +0,0 @@
-# WeatherAppRepo
-Ryder and Gregory will be creating a brilliant Weather App using bootstrap to style and using switch statements for the state of the App.
---
+++ WeatherApp/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { SearchBar } from './components/SearchBar'
+
+export default function App() {
+
+  return (
+    <>
+      {/* Code */}
+      <SearchBar></SearchBar>
+    </>
+  )
+}
